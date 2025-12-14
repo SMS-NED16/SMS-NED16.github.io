d9591246a0115128dac7dcbff9429302727adadb
Siddiqui, Saad Mashkoor <ssiddiqui60@gatech.edu>
2025-12-14 16:36:10 -0500

    First attempt at configuring deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://SMS-NED16.github.io', // Replace YOUR_USERNAME with your GitHub username
+  base: '/dev_portfolio_v1', // Replace with your repository name, or use '/' if repo name is YOUR_USERNAME.github.io
   vite: {
     plugins: [tailwindcss()],
   },
